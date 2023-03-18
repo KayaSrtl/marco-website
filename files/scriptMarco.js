1e4c35d57e0a6bd03df18dfe3485a11de8309d9f
KayaSrtl <50380412+KayaSrtl@users.noreply.github.com>
2023-03-18 22:05:04 +0300

    Update scriptMarco.js

--- files/scriptMarco.js
+++ files/scriptMarco.js
@@ -1,30 +1,66 @@
+var window_height, window_width;
+var ismenuopen = 0;
+
+
 $( document ).ready(function() {
-
-
-
+	beReadyPage();
+	
+	
 });
 
 
 $(document).keydown(function(e){
-    
-    
+	
+	
 });
 
 
 
 
 $(window).scroll(function(event){
-  
-  
-  
-  
+	
+	
+	
+	
 });
 
 
 function beReadyPage () {
 	window_height = parseInt($( window ).height());
 	window_width = parseInt($( window ).width());
-  
+	$(".fixed_menu_logo_part").css("left", ((parseInt($( ".fixed_menu_right_cont" ).width()) - parseInt($( ".fixed_menu_left_cont" ).width()))) / 2);
+	
+}
+
+
+function openLeftMenu () {
+	$(".fixed_menu_all_buttons_cont").stop();
+	$(".menu_closer").stop();
+	$('.fixed_menu_all_buttons_cont').animate(
+		{ left: ismenuopen ? -150 : 0 }, 200);
+	if(ismenuopen) {
+		$(".menu_closer").fadeOut(200);
+		$(".menu_opener").addClass('fa-bars');
+		$(".menu_opener").addClass('fa');
+		
+		$(".menu_opener").removeClass('fa-regular');
+		$(".menu_opener").removeClass('fa-solid');
+		$(".menu_opener").removeClass('fa-xmark');
+	}
+	else {
+		$(".menu_closer").fadeIn(200);
+		$(".menu_opener").removeClass('fa-bars');
+		$(".menu_opener").removeClass('fa');
+		
+		$(".menu_opener").addClass('fa-regular');
+		$(".menu_opener").addClass('fa-solid');
+		$(".menu_opener").addClass('fa-xmark');
+	}
+	//fa-regular fa-solid fa-xmark
+
+	ismenuopen = !ismenuopen;
+
+	//menu_closer
 }
 
 
